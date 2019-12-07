0378765eeee7f9053c53cf93e265b9228a2cc423
Gilbert D. Cuerbo <gilbert.cuerbo@detailonline.tech>
2019-12-08 00:23:46 +0800

    Requirements(1) create management command that saves all the pokemons from the pokeapi in the app pokedex database

--- pokepolls/management/commands/initcontent.py
+++ pokepolls/management/commands/initcontent.py
@@ -1,6 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
-import pokebase as pb
 import requests
+import time
+import re
+
+from pokepolls.models import Pokemon
 
 BASE_API = 'https://pokeapi.co/api/v2'
 # from polls.models import Question as Poll
@@ -13,16 +16,88 @@ class Command(BaseCommand):
        pass
 
     def handle(self, *args, **options):
-        self.stdout.write('Note! this comand will takes minutes to complete because of pokeapi limitations.')
-
+        pokemons = []
+        self.stdout.write('Note! this comand will takes minutes to complete due to pokeapi limitations.')
+        time.sleep(2)
+        self.stdout.write('- Fetching Pokemons')
+        # request all 1 generation pokemons
         try:
-            pokemons = requests.get(f'{ BASE_API }/generation/1/')
+            pokeInfo = requests.get(f'{ BASE_API }/generation/1/')
+            pokeInfo = pokeInfo.json()
+            pokeInfo = pokeInfo['pokemon_species']
 
-            # pokemons = pb.NamedAPIResource('berry', 'cherry')
-            pokemons = pokemons.json()
-            pokemons = pokemons['pokemon_species']
-            print(len(pokemons))
+            for poke in pokeInfo:
+                # request information of a particular pokemon
+                try:
+                    # initiate variables
+                    poke_id = int(re.split('\/', poke['url'])[-2])
+                    name = ''
+                    height = ''
+                    weight = ''
+                    image = ''
+                    held_items = '' #[]
+                    abilities = '' #[]
+                    types = '' #[]
+                    stats = '' #[]
+
+                    pokemon = requests.get(f'{ BASE_API }/pokemon/{ poke_id }/')
+                    pokemon = pokemon.json()
+
+                    # extract only the information use in this app
+                    # basic information
+                    name = pokemon['name']
+                    height = pokemon['height']
+                    weight = pokemon['weight']
+                    image = pokemon['sprites']['front_default']
+
+                    # stringified list of information
+                    held_items = ','.join(d['item']['name'] for d in pokemon['held_items'])
+                    abilities = ','.join(d['ability']['name'] for d in pokemon['abilities'])
+                    types = ','.join(d['type']['name'] for d in pokemon['types'])
+                    stats = ','.join(f"{ d['stat']['name'] }:{ d['base_stat'] }:{ d['effort'] }" for d in pokemon['stats'])
+
+                    pokemons.append({
+                        'poke_id': poke_id,
+                        'name': name,
+                        'height': height,
+                        'weight': weight,
+                        'image': image,
+                        'held_items': held_items,
+                        'abilities': abilities,
+                        'types': types,
+                        'stats': stats
+                    })
+                    time.sleep(0.5)
+
+                except:
+                    pass
+
+            self.stdout.write('  +-> Done')
 
         except:
-            self.stderr.write('Error fetching the pokemons!')
-        
+            self.stderr.write('  +-> Error fetching the pokemons!')
+
+        # save the pokemons to the model
+        if pokemons and len(pokemons):
+            self.stdout.write('- Saving all the pokemons')
+            poke_objs = []
+            for poke in pokemons:
+                poke_objs.append(Pokemon(
+                    poke_id=    poke['poke_id'],
+                    name=       poke['name'],
+                    height=     poke['height'],
+                    weight=     poke['weight'],
+                    image=      poke['image'],
+                    held_items= poke['held_items'],
+                    abilities=  poke['abilities'],
+                    types=      poke['types'],
+                    stats=       poke['stats']
+                ))
+
+            Pokemon.objects.bulk_create(poke_objs)
+            self.stdout.write('  +-> Done')
+
+        else:
+            self.stderr.write('No Pokemon to be save!')
+
+        self.stdout.write('End of the Process...')
