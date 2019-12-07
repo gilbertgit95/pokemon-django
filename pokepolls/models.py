be0073a07c1753144cdd5140e745503ee2677c79
Gilbert D. Cuerbo <gilbert.cuerbo@detailonline.tech>
2019-12-08 00:21:27 +0800

    setup model and other related proccess

--- pokepolls/models.py
+++ pokepolls/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+
+    poke_id = models.PositiveIntegerField()
+    name = models.CharField(max_length=50)
+    height = models.FloatField()
+    weight = models.FloatField()
+    image = models.CharField(max_length=500)
+    held_items = models.CharField(max_length=500)
+    abilities = models.CharField(max_length=500)
+    types = models.CharField(max_length=500)
+    stats = models.CharField(max_length=500)
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Pokemon'
