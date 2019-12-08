1c2365615c09678f5f05b24f105851acf2c71367
Gilbert D. Cuerbo <gilbert.cuerbo@detailonline.tech>
2019-12-09 07:14:32 +0800

    Requerments(3) Create a page that displays the detailes of a specific pokemon

--- pokepolls/urls.py
+++ pokepolls/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.PokemonView.as_view(), name='index'),
+    re_path(r'^pokemons/(?P<name>[\w-]+)/$', views.DetailView.as_view(), name='detail')
 ]
--- pokepolls/views.py
+++ pokepolls/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core import serializers
 from django.views import View
 from .models import Pokemon
+import re
+import json
+
+def get_name_from_url(path):
+    p = re.compile(matching_slug_regex)
+    return p.search(path).group('name')
 
 class PokemonView(View):
-    def get(sef, request):
+    def get(self, request):
         pokemons = Pokemon.objects.all()
         pokemons = list(pokemons)
         return render(request, 'home.html', {'pokemons': pokemons})
+
+class DetailView(View):
+    def get(self, request, name):
+        poke = serializers.serialize('json', Pokemon.objects.filter(name=name))
+        poke = json.loads(poke)[0]
+        poke = poke['fields']
+
+        poke['abilities'] = poke['abilities'].replace(',', ', ')
+        poke['held_items'] = poke['held_items'].replace(',', ', ')
+        poke['types'] = poke['types'].replace(',', ', ')
+
+        poke['stats'] = poke['stats'].split(',')
+        poke['stats'] = [item.split(':') for item in poke['stats']]
+
+        return render(request, 'pokemon.html', {'pokemon': poke})
