9625ed9c2e47edbdbf28bfe1c5e90cb7fa7126a6
kagiwaara <gilbert.cuerbo@gmail.com>
2019-12-08 17:01:20 +0800

    setup files for the next requirments

--- pokepolls/views.py
+++ pokepolls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+class PokemonView(View):
+    def get(sef, request):
+        return render(request, 'home.html')
