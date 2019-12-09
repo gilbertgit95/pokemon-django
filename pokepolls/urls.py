d2d10134d164a77bf4c3435ab269eace096403e1
kagiwaara <gilbert.cuerbo@gmail.com>
2019-12-09 23:21:09 +0800

    Requirements(4) Create a page that allows you to create a new pokemon

--- pokepolls/urls.py
+++ pokepolls/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.PokemonView.as_view(), name='index'),
+    path('add-pokemon', views.AddView.as_view(), name='add'),
     re_path(r'^pokemons/(?P<name>[\w-]+)/$', views.DetailView.as_view(), name='detail')
 ]
--- pokepolls/views.py
+++ pokepolls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.core import serializers
 from django.views import View
@@ -30,3 +30,60 @@ class DetailView(View):
         poke['stats'] = [item.split(':') for item in poke['stats']]
 
         return render(request, 'pokemon.html', {'pokemon': poke})
+
+class AddView(View):
+    def get(self, request):
+        poke = {}
+        poke['name'] = 'Enter name here'
+        poke['weight'] = 0
+        poke['height'] = 0
+        poke['image'] = ''
+        poke['abilities'] = []
+        poke['held_items'] = []
+        poke['types'] = []
+
+        poke['stats'] = [
+            ['speed', 0, 0],
+            ['special-defence', 0, 0],
+            ['special-attack', 0, 0],
+            ['defence', 0, 0],
+            ['attack', 0, 0],
+            ['hp', 0, 0]
+        ]
+
+        back_path = '/'
+        action = '/add-pokemon' # or modify
+
+        return render(request, 'poke-form.html', {'pokemon': poke, 'back_path': back_path, 'action': action })
+
+
+    def post(self, request):
+        try:
+            name = request.POST.get('name')
+            weight = request.POST.get('weight')
+            height = request.POST.get('height')
+            pokeid = request.POST.get('pokeid')
+            abilities = request.POST.get('abilities')
+            items = request.POST.get('items')
+            types = request.POST.get('types')
+            stats = request.POST.get('stats')
+            image = request.POST.get('image')
+
+            pokemon = Pokemon(
+                poke_id=pokeid,
+                name=name,
+                weight=weight,
+                height=height,
+                image=image,
+                held_items=items,
+                abilities=abilities,
+                types=types,
+                stats=stats
+            )
+            pokemon.save()
+
+        except:
+            console.log('Error saving the product')
+        
+
+        return redirect('/')
